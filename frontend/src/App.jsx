033faefe728209d8212cf0789dd97e62fcd6cde3
ReituTheCompSciGirlie <nbuhlereitumetse@gmail.com>
2025-07-29 19:05:52 +0200

    done with the tests page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from '../src/pages/Auth/LoginPage';
 import Dashboard from '../src/pages/Alert/Dashboard'; 
 import Register from '../src/pages/Auth/ResgisterPage';
 import Profile from '../src/pages/Profile/Profile';
+import Test from '../src/pages/Crud/DiagnosticTestList';
 
 function App() {
   return (
@@ -15,8 +15,9 @@ function App() {
         {/* Auth routes */}
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/test" element={<Test />} />
         {/* Protected route (add auth wrapper later if needed) */}
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
